c827f5c98446a0d4f00f21c5e7a2df24b6b8d582
Lorens Gueron <gueronlj@gmail.com>
2024-03-08 15:25:14 -0500

    Refactor: Tokens exchange

--- src/components/PlaidLink/PlaidLink.jsx
+++ src/components/PlaidLink/PlaidLink.jsx
@@ -5,30 +5,30 @@ import { usePlaidLink } from 'react-plaid-link';
 const PlaidLink = ({ linkToken }) => {
     const onSuccess = useCallback((public_token) => {
         // send public_token to server
-        fetch('http://localhost:3000/api/set_access_token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ public_token }),
-        })
-        const response = fetch('http://localhost:3000/api/exchange_public_token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ public_token }),
-        });
-
-        console.log(response);
-
+        const exchangePublicToken = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/api/exchange_public_token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ public_token: public_token }),
+            });
+            const data = await response.json();
+            console.log({
+                item_id: data.item_id,
+            });
+            } catch (error) {
+                console.error(error);
+            }           
+        }
+        exchangePublicToken();
     }, []);
 
     const config = {
         token: linkToken,
         onSuccess,
     };
-
     const { open, ready } = usePlaidLink(config);
 
     return (
